baaa2ad19f1c0a48445e5d9d85fb344af6faa54a
Saurav Sahu <mrsauravsahu@outlook.com>
2023-02-03 12:54:13 +0530

    add health check for consumer at root

--- app/consumer/src/app.controller.ts
+++ app/consumer/src/app.controller.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @MessagePattern('number')
   getNumber(@Payload() message: any): void {
     console.log(message);
+  }
+
+  @Get()
+  getAbout(): { message: string } {
+    return { message: 'Consumer service is running' };
   }
 }
